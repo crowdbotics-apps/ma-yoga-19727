633d1fa591711fbd26703e745333ce636c2672d4
Crowdbotics <team@crowdbotics.com>
2020-08-22 13:53:21 +0000

    Initial commit.

---
+++ backend/ma_yoga_19727/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ma_yoga_19727 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ma_yoga_19727.settings')
+
+application = get_wsgi_application()
---
+++ ios/ma_yoga_19727Tests/ma_yoga_19727Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface ma_yoga_19727 : XCTestCase
+
+@end
+
+@implementation ma_yoga_19727
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
